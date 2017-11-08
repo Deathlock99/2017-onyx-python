1f9ee4d5be0c42a50947755dd8b24dfd90a3a367
jensen3584d <jensen3584d@ckstudents.org>
2017-11-07 16:59:43 -0800

    hello world

--- src/robot.py
+++ src/robot.py
@@ -7,15 +7,46 @@ class Onyx(wpilib.IterativeRobot):
     
     def robotInit(self):
         '''Robot-wide initialization code should go here'''
-        pass
+        self.motor=wpilib.Victor(1)
+        self.motor2=wpilib.Victor(2)
+        self.motor3=wpilib.Victor(3)
+        self.motor4 = wpilib.Victor(4)
+        self.state="Drive"
+        self.timer=wpilib.Timer()
+        self.drive=wpilib.RobotDrive
 
     def autonomousInit(self):
         '''Called only at the beginning of autonomous mode'''
-        pass
+        self.timer.start()
 
     def autonomousPeriodic(self):
         '''Called every 20ms in autonomous mode'''
-        pass
+        if self.state == "Drive":
+            self.motor.set(1)
+            self.motor2.set(1)
+            if self.timer.get()>2:
+                self.state="LTurn"
+        elif self.state == "LTurn":
+            self.motor.set(0)
+            if self.timer.get()>5.1:
+                self.state="Drive2"
+        elif self.state == "Drive2":
+            self.motor.set(1)
+            self.motor2.set(1)
+            if self.timer.get()>7.1:
+                self.state="RTurn"
+        elif self.state == "RTurn":
+            self.motor2.set(0)
+            if self.timer.get()>10.2:
+                self.state="Drive3"
+        elif self.state == "Drive3":
+            self.motor2.set(1)
+            if self.timer.get()>12.2:
+                self.state="Stop"
+        elif self.state == "Stop":
+            self.motor.set(0)
+            self.motor2.set(0)
+
 
     def disabledInit(self):
         '''Called only at the beginning of disabled mode'''
